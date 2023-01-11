52500f38dc4befc491a14e6a5c9e26c71e62a926
Hanatan <hanatan079@gmail.com>
2023-01-11 09:39:01 +0900

    変数作成
    移動スピード、ジャンプ力、rigidbody格納

--- Run/Assets/Scriptes/Player.cs
+++ Run/Assets/Scriptes/Player.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    //移動スピード、ジャンプ力
+    [SerializeField]
+    private float moveSpeed = 7f, jumpSpeed = 5f;
+
+    //rigidBody格納
+    private Rigidbody2D rd;
+
+    
+    private float 
     // Start is called before the first frame update
     void Start()
     {
