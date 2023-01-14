503862a9311d009b13fa448f2a172c6114d61944
Hanatan <hanatan079@gmail.com>
2023-01-14 15:50:48 +0900

    障害物格納用

--- Run/Assets/Scriptes/Obstacle.cs
+++ Run/Assets/Scriptes/Obstacle.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
+    //障害物格納用
+    private GameObject Obstacle1, Obstacle2, Obstacle3, Obstacle4;
+
+
+
+    
     // Start is called before the first frame update
     void Start()
     {
