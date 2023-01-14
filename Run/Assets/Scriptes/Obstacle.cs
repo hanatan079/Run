37d07002968f07641ec02f4ef321a33c2c5d6720
Hanatan <hanatan079@gmail.com>
2023-01-14 15:57:22 +0900

    障害物生成時Y軸管理用

--- Run/Assets/Scriptes/Obstacle.cs
+++ Run/Assets/Scriptes/Obstacle.cs
@@ -7,9 +7,13 @@ public class Obstacle : MonoBehaviour
     //障害物格納用
     private GameObject Obstacle1, Obstacle2, Obstacle3, Obstacle4;
 
+    //障害物生成時Y軸管理用
+    [SerializeField]
+    private void Obstacle1Pos = -4, Obstacle2pos = -4, Obstacle3Pos = -4, Obstacle4Pos = -4;
 
 
-    
+
+
     // Start is called before the first frame update
     void Start()
     {
