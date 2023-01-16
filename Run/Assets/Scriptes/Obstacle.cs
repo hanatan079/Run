ef24bcfa032727eff0c9abfec3e28799acfa9821
Hanatan <hanatan079@gmail.com>
2023-01-16 10:25:29 +0900

    変数名修正

--- Run/Assets/Scriptes/Obstacle.cs
+++ Run/Assets/Scriptes/Obstacle.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class Obstacle : MonoBehaviour
 {
     //障害物格納用
-    private GameObject Obstacle1, Obstacle2, Obstacle3, Obstacle4;
+    private GameObject obstacle1, obstacle2, obstacle3, obstacle4;
 
     //障害物生成時Y軸管理用
     [SerializeField]
-    private float Obstacle1Pos = -4, Obstacle2pos = -4, Obstacle3Pos = -4, Obstacle4Pos = -4;
+    private float obstacle1Pos = -4, obstacle2pos = -4, obstacle3Pos = -4, obstacle4Pos = -4;
 
     //スポーン時の数値格納用(ランダムに障害物を発生させる)
     private float minTime = 2f, maxTime = 3.5f;
@@ -19,7 +19,7 @@ public class Obstacle : MonoBehaviour
     private float spawnTime;
 
     //障害物の種類を扱う(4種類=4)
-    private int ObstacleType = 4;
+    private int obstacleType = 4;
 
     //生成するオブジェクト
     private int obstacleSpawn;
