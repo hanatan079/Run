9ac184a099ffdd02f9a422bb9af5b346d91c8f70
Hanatan <hanatan079@gmail.com>
2023-01-19 09:47:41 +0900

    障害物を回収する

--- Run/Assets/Scriptes/Collect.cs
+++ Run/Assets/Scriptes/Collect.cs
@@ -7,7 +7,7 @@ public class Collect : MonoBehaviour
     //床へぶつかったら非表示にする
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("Floor"))
+        if(collision.CompareTag("Floor") || collision.CompareTag("obstacle") )
         {
             collision.gameObject.SetActive(false);
         }
